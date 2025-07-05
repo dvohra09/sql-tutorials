8c3f2619affe28f05913e5dd8e19d5a640580cfe
dvohra09 <dvohra09@yahoo.com>
2025-07-04 17:47:03 -0700

    Update example-sql-statements-that-generate-error-message.sql

--- sql-queries-11/fix-error-1064-when-using-ALTER-TABLE-ADD CONSTRAINT-in-MySQL/example-sql-statements-that-generate-error-message.sql
+++ sql-queries-11/fix-error-1064-when-using-ALTER-TABLE-ADD CONSTRAINT-in-MySQL/example-sql-statements-that-generate-error-message.sql
@@ -44,7 +44,7 @@ REFERENCES Program ("id");
 
 # Correct syntax; backticks
 ALTER TABLE `Specification`
-ADD CONSTRAINT specification_program_id_fkey 
+ADD CONSTRAINT `specification_program_id_fkey` 
 FOREIGN KEY (`program_id`)
 REFERENCES Program (`id`);
 
